43ed708a56110179c53d617beaf0e321b1040866
GabozAlex <102251475+GabozAlex@users.noreply.github.com>
2024-10-20 08:02:37 -0400

    v1.01
    Mejora visual al inicio, y al codigo javascript

--- funciones.js
+++ funciones.js
@@ -1,222 +1,181 @@
-let colores = ["red", "yellow", "green", "blue", "gray"]
-let estados = ["APAGADO", "INTERMITENTE" , "ENCENDIDO"]
+let estado_foto=[
+	"Imagenes/bombillo_apagado.png","Imagenes/bombillo_rojo.png","Imagenes/bombillo_amarillo.png","Imagenes/bombillo_verde.png" ]
+let estados_semaforo=["APAGADO","INTERMITENTE","ENCENDIDO"]
 
-let on_intermitente = false
+let on_intermitente=false
+let off_intermitente=false
+
+let on_semaforo =false
+let off_semaforo =false
 
 let intervalo_intermitente
-let intervalo_recorrido
+let intervalo_semaforo
 
 let intervalo_norte_paso
-let intervalo_norte_reduzca
+let intervalo_norte_reduzco
 let intervalo_norte_stop
 
 let intervalo_este_paso
-let intervalo_este_reduzca
+let intervalo_este_reduzco
 let intervalo_este_stop
 
 let intervalo_sur_paso
-let intervalo_sur_reduzca
+let intervalo_sur_reduzco
 let intervalo_sur_stop
 
 let intervalo_oeste_paso
-let intervalo_oeste_reduzca
+let intervalo_oeste_reduzco
 let intervalo_oeste_stop
 
-function inicializar_semaforo(){
-	document.getElementById("rojo").style.background = colores[4]
-	document.getElementById("amarillo").style.background = colores[4]
-	document.getElementById("verde").style.background = colores[4]
-	document.getElementById("naranja").style.background = colores[4]
-	document.getElementById("azul").style.background = colores[4]
-
-	document.getElementById("rojo1").style.background = colores[4]
-	document.getElementById("amarillo1").style.background = colores[4]
-	document.getElementById("verde1").style.background = colores[4]
-	document.getElementById("naranja1").style.background = colores[4]
-	document.getElementById("azul1").style.background = colores[4]
-
-	document.getElementById("rojo2").style.background = colores[4]
-	document.getElementById("amarillo2").style.background = colores[4]
-	document.getElementById("verde2").style.background = colores[4]
-	document.getElementById("naranja2").style.background = colores[4]
-	document.getElementById("azul2").style.background = colores[4]
-
-	document.getElementById("rojo3").style.background = colores[4]
-	document.getElementById("amarillo3").style.background = colores[4]
-	document.getElementById("verde3").style.background = colores[4]
-	document.getElementById("naranja3").style.background = colores[4]
-	document.getElementById("azul3").style.background = colores[4]
-}
-
-function intermitente(){
-
-	if(on_intermitente==false){
-		document.getElementById("amarillo").style.background = colores[1]
-		document.getElementById("amarillo1").style.background = colores[1]
-		document.getElementById("amarillo2").style.background = colores[1]
-		document.getElementById("amarillo3").style.background = colores[1]
-		document.getElementById("amarillo3").style.background = colores[1]
-		on_intermitente=true	
-	}else{
-		document.getElementById("amarillo").style.background = colores[4]
-		document.getElementById("amarillo1").style.background = colores[4]
-		document.getElementById("amarillo2").style.background = colores[4]
-		document.getElementById("amarillo3").style.background = colores[4]
-		document.getElementById("amarillo3").style.background = colores[4]
-		on_intermitente=false
-	}
-	
-}
-
-function encender_intermitente(){
-	inicializar_semaforo()
-	detener_intervalos()
-
-	intervalo_intermitente = setInterval(intermitente,1000)
-	document.getElementById('texto').textContent=estados[1]
-}
-
-function apagar_semaforo(){
-	inicializar_semaforo()
-	detener_intervalos()
-	document.getElementById('texto').textContent=estados[0]	
+function inicializar(){
+	document.getElementById("norte").src=estado_foto[0]
+	document.getElementById("este").src=estado_foto[0]
+	document.getElementById("sur").src=estado_foto[0]
+	document.getElementById("oeste").src=estado_foto[0]
 }
 
 function norte_paso(){
-	document.getElementById("rojo").style.background = colores[4]
-	document.getElementById("amarillo").style.background = colores[4]
-	document.getElementById("verde").style.background = colores[2]
-	document.getElementById("azul").style.background = colores[4]
+	document.getElementById("norte").src=estado_foto[3]
 
 	este_stop()
 	sur_stop()
 	oeste_stop()
 }
-
 function norte_reduzca(){
-	document.getElementById("amarillo").style.background = colores[1]
-	document.getElementById("verde").style.background = colores[4]
+	document.getElementById("norte").src=estado_foto[2]
 }
-
 function norte_stop(){
-	document.getElementById("rojo").style.background = colores[0]
-	document.getElementById("amarillo").style.background = colores[4]
+	document.getElementById("norte").src=estado_foto[1]
 }
 
 function este_paso(){
-	document.getElementById("rojo1").style.background = colores[4]
-	document.getElementById("amarillo1").style.background = colores[4]
-	document.getElementById("verde1").style.background = colores[2]
-	document.getElementById("azul1").style.background = colores[4]
+	document.getElementById("este").src=estado_foto[3]
 
 	norte_stop()
 	sur_stop()
 	oeste_stop()
 }
-
 function este_reduzca(){
-	document.getElementById("amarillo1").style.background = colores[1]
-	document.getElementById("verde1").style.background = colores[4]
+	document.getElementById("este").src=estado_foto[2]
 }
-
 function este_stop(){
-	document.getElementById("rojo1").style.background = colores[0]
-	document.getElementById("amarillo1").style.background = colores[4]
+	document.getElementById("este").src=estado_foto[1]
 }
 
 function sur_paso(){
-	document.getElementById("rojo2").style.background = colores[4]
-	document.getElementById("amarillo2").style.background = colores[4]
-	document.getElementById("verde2").style.background = colores[2]
-	document.getElementById("azul2").style.background = colores[4]
+	document.getElementById("sur").src=estado_foto[3]
 
 	norte_stop()
 	este_stop()
 	oeste_stop()
 }
-
 function sur_reduzca(){
-	document.getElementById("amarillo2").style.background = colores[1]
-	document.getElementById("verde2").style.background = colores[4]
+	document.getElementById("sur").src=estado_foto[2]
 }
-
 function sur_stop(){
-	document.getElementById("rojo2").style.background = colores[0]
-	document.getElementById("amarillo2").style.background = colores[4]
+	document.getElementById("sur").src=estado_foto[1]
 }
 
 function oeste_paso(){
-	document.getElementById("rojo3").style.background = colores[4]
-	document.getElementById("amarillo3").style.background = colores[4]
-	document.getElementById("verde3").style.background = colores[2]
-	document.getElementById("azul3").style.background = colores[4]
+	document.getElementById("oeste").src=estado_foto[3]
 
 	norte_stop()
 	este_stop()
 	sur_stop()
 }
-
 function oeste_reduzca(){
-	document.getElementById("amarillo3").style.background = colores[1]
-	document.getElementById("verde3").style.background = colores[4]
+	document.getElementById("oeste").src=estado_foto[2]
 }
-
 function oeste_stop(){
-	document.getElementById("rojo3").style.background = colores[0]
-	document.getElementById("amarillo3").style.background = colores[4]
+	document.getElementById("oeste").src=estado_foto[1]
 }
-
-
 
 function recorrido_semaforo(){
-	intervalo_norte_paso =setTimeout(norte_paso,1000)
-	intervalo_norte_reduzca =setTimeout(norte_reduzca,4000)
-	intervalo_norte_stop =setTimeout(norte_stop,7000)
+	intervalo_norte_paso = setTimeout(norte_paso,1000)
+	intervalo_norte_reduzco = setTimeout(norte_reduzca,4000)//3
+	intervalo_norte_stop = setTimeout(norte_stop,7000)//3
 
-	intervalo_este_paso =setTimeout(este_paso,10000)
-	intervalo_este_reduzca =setTimeout(este_reduzca,13000)
-	intervalo_este_stop =setTimeout(este_stop,16000)
+	intervalo_este_paso = setTimeout(este_paso,7500)//0.5
+	intervalo_este_reduzco = setTimeout(este_reduzca,10000)//3
+	intervalo_este_stop = setTimeout(este_stop,13000)//3
 
-	intervalo_sur_paso =setTimeout(sur_paso,19000)
-	intervalo_sur_reduzca =setTimeout(sur_reduzca,22000)
-	intervalo_sur_stop =setTimeout(sur_stop,25000)
+	intervalo_sur_paso = setTimeout(sur_paso,13500)//0.5
+	intervalo_sur_reduzco = setTimeout(sur_reduzca,16000)//3
+	intervalo_sur_stop = setTimeout(sur_stop,19000)//3
 
-	intervalo_oeste_paso =setTimeout(oeste_paso,28000)
-	intervalo_oeste_reduzca =setTimeout(oeste_reduzca,31000)
-	intervalo_oeste_stop =setTimeout(oeste_stop,34000)
+	intervalo_oeste_paso = setTimeout(oeste_paso,19500)//0.5
+	intervalo_oeste_reduzco = setTimeout(oeste_reduzca,22000)//3
+	intervalo_oeste_stop =setTimeout(oeste_stop,25000)//3
 }
 
-function encender_semaforo(){
-
-	detener_intervalos()
-	inicializar_semaforo()
-	recorrido_semaforo()
-
-	intervalo_recorrido=setInterval(recorrido_semaforo,33000)
-
-	document.getElementById('texto').textContent=estados[2]
+function intermitente(){
+	if(on_intermitente==false){
+		on_intermitente=true
+		document.getElementById("norte").src=estado_foto[2]
+		document.getElementById("este").src=estado_foto[2]
+		document.getElementById("sur").src=estado_foto[2]
+		document.getElementById("oeste").src=estado_foto[2]		
+	}else{
+		on_intermitente=false
+		document.getElementById("norte").src=estado_foto[0]
+		document.getElementById("este").src=estado_foto[0]
+		document.getElementById("sur").src=estado_foto[0]
+		document.getElementById("oeste").src=estado_foto[0]		
+	}
 }
 
 function detener_intervalos(){
 	clearTimeout(intervalo_norte_paso)
-	clearTimeout(intervalo_norte_reduzca)
+	clearTimeout(intervalo_norte_reduzco)
 	clearTimeout(intervalo_norte_stop)
 
 	clearTimeout(intervalo_este_paso)
-	clearTimeout(intervalo_este_reduzca)
+	clearTimeout(intervalo_este_reduzco)
 	clearTimeout(intervalo_este_stop)
 
 	clearTimeout(intervalo_sur_paso)
-	clearTimeout(intervalo_sur_reduzca)
+	clearTimeout(intervalo_sur_reduzco)
 	clearTimeout(intervalo_sur_stop)
 
 	clearTimeout(intervalo_oeste_paso)
-	clearTimeout(intervalo_oeste_reduzca)
+	clearTimeout(intervalo_oeste_reduzco)
 	clearTimeout(intervalo_oeste_stop)
 
 	clearInterval(intervalo_intermitente)
-	clearInterval(intervalo_recorrido)
+	clearInterval(intervalo_semaforo)
+}
 
+function encender_semaforo(){
+	document.getElementById("texto").textContent = estados_semaforo[2]
+	inicializar()
+	
+	if(on_semaforo == false){
+		on_semaforo =true
+		detener_intervalos()
+		recorrido_semaforo()
+		intervalo_semaforo = setInterval(recorrido_semaforo,25500)
+	}else{
+		detener_intervalos()
+		recorrido_semaforo()
+		intervalo_semaforo = setInterval(recorrido_semaforo,25500)
+	}
+}
+
+function encender_intermitente(){
+	document.getElementById("texto").textContent = estados_semaforo[1]
+	if(on_semaforo == true){
+		detener_intervalos()
+		intervalo_intermitente = setInterval(intermitente,1000)	
+	}else{
+		intervalo_intermitente = setInterval(intermitente,1000)
+	}
+	
+}
+
+function apagar_semaforo(){
+	document.getElementById("texto").textContent = estados_semaforo[0]
+	inicializar()
+	detener_intervalos()
 }
 
 document.getElementById("intermitente").addEventListener('click', encender_intermitente)
